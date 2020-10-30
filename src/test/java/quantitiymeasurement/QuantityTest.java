b6b2c8c7caf0542c4a3c2aa53b87de03555e806f
Bhagi <puppyreddy1997@gmail.com>
2020-10-30 14:18:24 +0530

    [Bhagyalaxmi]Added:Added code and Test to check equality of 2 Inch variables

--- src/test/java/quantitiymeasurement/QuantityTest.java
+++ src/test/java/quantitiymeasurement/QuantityTest.java
@@ -10,6 +10,25 @@ public class QuantityTest {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
-
+    }
+    @Test
+    public void given0Feet1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
+    }
+    @Test
+    public void given0Inch0Inch_ShouldReturnEqual(){
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+    @Test
+    public void given0Inch1Inch_ShouldReturnEqual(){
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        boolean equals = inch1.equals(inch2);
+        Assert.assertFalse(equals);
     }
 }
